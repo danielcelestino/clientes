55467b02350814649e0dd18ae900e33601038ccd
Daniel Celestino <danielcelestino@gmail.com>
2022-11-12 17:06:27 -0300

    Add @CrossOrigin to permit call from localhost:4200

--- src/main/java/br/com/daniel/rest/ClienteController.java
+++ src/main/java/br/com/daniel/rest/ClienteController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     private ClienteRepository repository;
